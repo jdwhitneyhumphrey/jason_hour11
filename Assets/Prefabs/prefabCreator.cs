daff920864cbb08b68baf5b02a8c761174442461
jdwhitneyhumphrey <124301860+jdwhitneyhumphrey@users.noreply.github.com>
2023-04-20 11:42:11 -0700

    Figure design

--- Assets/Prefabs/prefabCreator.cs
+++ Assets/Prefabs/prefabCreator.cs
@@ -9,7 +9,7 @@ public class prefabCreator : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 1; i <= 5; i++)
+        for (int i = 1; i <= 10; i++)
         {
             Vector3 spawnposition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 1f, Random.Range(-spawnValues.z, spawnValues.z));
             GameObject gameobject = Instantiate<GameObject>(Figure, spawnposition, Quaternion.identity);
